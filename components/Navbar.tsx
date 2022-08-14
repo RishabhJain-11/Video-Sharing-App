e9c906ffbb18188667b99d61091ccadba9df2463
Rishabh Jain <jainrishabh71011@gmail.com>
2022-08-14 18:51:27 +0530

    ..

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,17 +5,25 @@ import { useRouter } from 'next/router';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
-import { GoogleLogin, googleLogout  } from '@react-oauth/google';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 
-// import useAuthStore from '../store/authStore';
-// import { IUser } from '../types';
-import { createOrGetUser } from '../utils';
 import Logo from '../utils/tiktik-logo.png';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+            <Link href="/">
+                <div className='w-[100px] md:w-[130px]'>
+                    <Image
+                        className='cursor-pointer'
+                        src={Logo}
+                        alt="Tiktik"
+                        layout='responsive'
+                    />
+                </div>
+            </Link>
+        </div>
+    )
 }
 
 export default Navbar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,13 +16,13 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
   return (
     <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
-      Navbar
+      <Navbar />
       <div className='flex gap-6 md:gap-20'>
         <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
-          Sidebar
+          <Sidebar />
         </div>
         <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh]videos flex-1'>
-          Component
+          {/* Component */}
         </div>
       </div>
     </div>
