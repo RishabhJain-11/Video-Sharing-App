b7b7bb4093046c3431087c7c21a01c6231872123
Rishabh Jain <jainrishabh71011@gmail.com>
2022-08-16 19:07:49 +0530

    ..

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,39 @@
-import type { NextPage } from 'next';
+import React from 'react';
+import axios from 'axios';
 
-const Home: NextPage = () => {
-  return (
-    <h1 className='text-3xl  underline'>
-      Hello World!
-    </h1>
-  )
+import VideoCard from '../components/VideoCard';
+import { BASE_URL } from '../utils';
+import { Video } from '../types';
+import NoResults from '../components/NoResults';
+
+interface IProps {
+  videos: Video[];
 }
 
+
+const Home = ({ videos }: IProps) => {
+  return (
+    <div className='flex flex-col gap-10 videos h-full'>
+      {videos.length 
+        ? videos?.map((video: Video) => (
+          <VideoCard post={video}  key={video._id} />
+        )) 
+        : <NoResults text={`No Videos`} />}
+    </div>
+  );
+};
+
 export default Home;
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
+
+  return {
+    props: {
+      videos: data
+    }
+  }
+}
+
+
+
